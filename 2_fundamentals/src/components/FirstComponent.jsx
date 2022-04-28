0f08e862c634d99409cbcc3ecab1eb8ff8ea75f8
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-28 11:46:20 -0300

    Feature: :sparkles: Fundamentals module added

---
+++ 2_fundamentals/src/components/FirstComponent.jsx
@@ -0,0 +1,11 @@
+// Criamos por padrão uma arrow-function, e para isso colocamos dentro de uma constante
+const FirstComponent = () => {
+	return (
+		<div>
+			<h2>My First component</h2>
+		</div>
+	)
+}
+
+// Exportamos o component com o nome no qual iremos utilizar nas importações
+export default FirstComponent;
---
+++ 2_fundamentals/src/components/HelloButton.jsx
@@ -0,0 +1,9 @@
+const HelloButton = () => {
+	return (
+		<>
+			<button>Cliquei aqui para visualizar a mensagem</button>
+		</>
+	)
+}
+
+export default HelloButton;
