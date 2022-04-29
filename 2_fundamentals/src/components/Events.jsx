dbc67b4fc3e415bad5bd03e93c45b79f72bb7407
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-29 11:53:04 -0300

    Feature: :sparkles: Functions in events

--- 2_fundamentals/src/components/Events.jsx
+++ 2_fundamentals/src/components/Events.jsx
@@ -9,7 +9,24 @@ const Event = () => {
 		<>
 			<div>
 				{/* O evento normalmente vem sem os (), só utilizamos quando queremos que o evento ative automaticamente ao carregar a página */}
-				<button onClick={handleShowMessage}>Clique aqui!</button>
+				<button onClick={handleShowMessage}>Mensagem boas-vindas!</button>
+			</div>
+
+			<div>
+				{/* Quando formos utilizar uma função inline, precisamos utilizar dentro de uma arrow-function */}
+				<button onClick={() => console.log('Funfou!')}>Evento inline</button> <br />
+
+				{/* Não recomendado para funções mais complexas, segue um exemplo de má-prática*/}
+				<button onClick={() => {
+					const x = 0;
+					const currentName = 'John Doe'
+
+					if(x !== 1) {
+						console.log(`Olá ${currentName}`)
+					}
+				}}>
+					Má prática
+				</button>
 			</div>
 		</>
 	)
