0f3f0ed7058f0cfb7c789b2cf9095c48658038a7
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-04 11:50:00 -0300

    Feature: :sparkles: Source images

--- 3_advancing/src/components/ImagesTypes.jsx
+++ 3_advancing/src/components/ImagesTypes.jsx
@@ -1,9 +1,21 @@
+import WorkspaceImage from '../assets/Workspace.jpg'
+
 const ImagesTypes = () => {
 
 	return (
 		<>
-			{/* Imagem em public, para ter acesso é simples, basta colocar '/nomeDaImagem.jpg' */}
-			<img height="250" src="Nature.jpg" alt="Imagem da natureza" />
+			<div className="imagesDiv">
+				{/* Imagem em public, para ter acesso é simples, basta colocar '/nomeDaImagem.jpg' */}
+				<img 
+					height="250" 
+					src="Nature.jpg" 
+					alt="Imagem da natureza" 
+					style={{marginRight: `${32}px`}}
+				/>
+
+				{/* Imagem em asset */}
+				<img height="250" src={WorkspaceImage} alt="Área de trabalho" />
+			</div>
 		</>
 	)
 }
