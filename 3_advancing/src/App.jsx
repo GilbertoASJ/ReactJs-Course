aac0c1d47246fdf10f2dfe6e54ffe5c7851141f2
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-04 11:20:54 -0300

    Feature: :sparkles: Public images

---
+++ 3_advancing/src/App.jsx
@@ -0,0 +1,14 @@
+import ImagesTypes from './components/ImagesTypes' 
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h3>Seção 3 - Avançando em React</h3>
+            <ImagesTypes />
+        </div>
+    );
+}
+
+export default App;
---
+++ 3_advancing/src/components/ImagesTypes.jsx
@@ -0,0 +1,11 @@
+const ImagesTypes = () => {
+
+	return (
+		<>
+			{/* Imagem em public, para ter acesso é simples, basta colocar '/nomeDaImagem.jpg' */}
+			<img height="250" src="Nature.jpg" alt="Imagem da natureza" />
+		</>
+	)
+}
+
+export default ImagesTypes;
