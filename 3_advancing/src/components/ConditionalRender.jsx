823640e4d3b1eeaaebea2c74f70e07686e6aa518
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-05 15:44:21 -0300

    Feature: :sparkles: If/Else in JSX

--- 3_advancing/src/components/ConditionalRender.jsx
+++ 3_advancing/src/components/ConditionalRender.jsx
@@ -2,18 +2,41 @@ import { useState } from 'react'
  
 const ConditionalRender = () => {
 
-	const [authUser] = useState(false)
+	const [authUser] = useState(true)
+	const [userName, setUserName] = useState('Fulano de tal')
 
 	return (
 		<>	
-			{/* Condicional simples */}
-			<h3>Isso será exibido?</h3>
-			{authUser && <p>Se o usuário estiver logado sim!</p>}
+			<div>
+				{/* Condicional simples */}
+				<h3>Isso será exibido?</h3>
+				{authUser && <p>Se o usuário estiver logado sim!</p>}
 
 
-			{/* Invertendo o valor
-				{!authUser && <p>Se o usuário estiver logado sim!</p>}
-			*/}
+				{/* Invertendo o valor
+					{!authUser && <p>Se o usuário estiver logado sim!</p>}
+				*/}
+			</div>
+
+			<div>
+				{/* Condicional If/Else no JSX */}
+				{userName === 'John Doe' ? (
+
+					<div>
+						<p>O nome do usuário é John Doe</p>
+					</div>
+					
+				) : (
+
+					<div>
+						<p>Nome do usuário não informado</p>
+					</div>
+				)}
+
+				<button onClick={() => {setUserName('John Doe')}}>
+					Alterar nome
+				</button>
+			</div>
 		</>
 	)
 }
