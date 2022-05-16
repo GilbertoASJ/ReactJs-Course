176d69cd163f9637350add7a02746455e367bccf
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-16 11:08:28 -0300

    Feature: :sparkles: component css

--- 4_css/src/App.jsx
+++ 4_css/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 
+import ComponentCss from './components/ComponentCss/ComponentCss'
+
 function App() {
 
     return (
         <div className="App">
 
+            {/* CSS global */}
             <h3>React com CSS</h3>
+
+            {/* Css de componente - ele vaza, é como se fosse global, por isso utilizar com cuidado, 
+            sempre estilizando com classes ou id's */}
+            <ComponentCss />
         </div>
     );
 }
---
+++ 4_css/src/components/ComponentCss/ComponentCss.jsx
@@ -0,0 +1,16 @@
+import './ComponentCss.css'
+
+const ComponentCss = () => {
+
+	return (
+		<div className="principalContent">
+			<h3>CSS de componente</h3>
+			<p><strong>Este é um exemplo de css de componente.</strong></p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolores maxime, ipsa cumque velit optio beatae.
+			</p>
+		</div>
+	)
+}
+
+export default ComponentCss;
