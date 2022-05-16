8dbaa4fadc379b6c26bee03f898301137155b877
GilbertoASJ <alvesgilberto84@gmail.com>
2022-05-16 11:28:53 -0300

    Feature: :sparkles: Inline css

--- 4_css/src/App.jsx
+++ 4_css/src/App.jsx
@@ -13,6 +13,21 @@ function App() {
             {/* Css de componente - ele vaza, é como se fosse global, por isso utilizar com cuidado, 
             sempre estilizando com classes ou id's */}
             <ComponentCss />
+
+            {/* Inline CSS */}
+            <button 
+                onClick={() => alert('Seja bem vindo ao sistema')}
+                style={{
+                    border: 0,
+                    borderRadius: '8px',
+                    backgroundColor: '#222',
+                    color: '#fff',
+                    padding: '8px 16px',
+                    marginTop: '16px'
+                }}
+            >
+                Exibir mensagem de boas-vindas
+            </button>
         </div>
     );
 }
