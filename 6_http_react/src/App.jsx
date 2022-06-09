3df037ff3033fca9cd34ae14d08db625323841f4
GilbertoASJ <alvesgilberto84@gmail.com>
2022-06-09 14:05:23 -0300

    Feature: :sparkles: Adding data with r eact

--- 6_http_react/src/App.jsx
+++ 6_http_react/src/App.jsx
@@ -6,8 +6,10 @@ const url = 'http://localhost:3000/products'
 function App() {
 
     const [products, setProducts] = useState([])
+    const [productName, setProductName] = useState("")
+    const [productPrice, setProductPrice] = useState("")
 
-    // Resgatando dados
+    // 1 - Resgatando dados
     useEffect(() => {
 
         async function fetchData() {
@@ -22,6 +24,28 @@ function App() {
         
     }, [])
 
+    // 2 - Adicionando produtos
+    const handleSubmit = async (e) => {
+
+        e.preventDefault()
+
+        const product = {
+            name: productName,
+            price: productPrice
+        }
+
+        // Adicionando o produto no db.json
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(product)
+        })
+
+        console.log(product, products)
+    }
+
     return (
         <div className="App">
             <h2>Lista de produtos</h2>
@@ -34,6 +58,36 @@ function App() {
                     </li>
                 ))}
             </ul>
+
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Nome: 
+                        <input 
+                            type="text" 
+                            value={productName} 
+                            name="productName" 
+                            onChange={(e) => {setProductName(e.target.value)}} 
+                        />
+                    </label>
+
+                    <br /><br />
+
+                    <label>
+                        Preço: 
+                        <input 
+                            type="number" 
+                            value={productPrice} 
+                            name="productPrice" 
+                            onChange={(e) => {setProductPrice(e.target.value)}} 
+                        />
+                    </label>
+
+                    <br />
+
+                    <input type="submit" value="Criar produto" />
+                </form>
+            </div>                                    
         </div>
     );
 }
