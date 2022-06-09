e6bbbfe9e86c5aff206c24999681c25e13cc5db7
GilbertoASJ <alvesgilberto84@gmail.com>
2022-06-09 10:27:04 -0300

    Feature: :sparkles: Rescuing data from the API

--- 6_http_react/src/App.jsx
+++ 6_http_react/src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+
+const url = 'http://localhost:3000/products'
 
 function App() {
+
+    const [products, setProducts] = useState([])
+
+    // Resgatando dados
+    useEffect(() => {
+
+        async function fetchData() {
+
+            const res = await fetch(url)
+            const data = await res.json()
+
+            setProducts(data)
+        }
+        
+        fetchData()
+        
+    }, [])
+
     return (
         <div className="App">
             <h2>Lista de produtos</h2>
+
+            <ul>
+                {products.map((product) => (
+
+                    <li key={product.id}>
+                        {product.name} - R$: {product.price}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
