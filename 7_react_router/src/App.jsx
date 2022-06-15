ca516f2dfdb07b4bc16958f4fc168bfffa576800
GilbertoASJ <alvesgilberto84@gmail.com>
2022-06-15 12:17:18 -0300

    Feature: :sparkles: Page navigation

--- 7_react_router/src/App.jsx
+++ 7_react_router/src/App.jsx
@@ -6,11 +6,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 
+import Navbar from './components/Navbar'
+
 function App() {
     return (
         <div className="App">
             <h2>Produtos</h2>
+
             <BrowserRouter>
+
+                {/* Quando o componente possui elemento do react route, 
+                ele fica dentro de browserRouter */}
+                <Navbar />
+
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
