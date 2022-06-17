f6283c1b2e8b43b7c3eca576b682c76bc82f84cd
GilbertoASJ <alvesgilberto84@gmail.com>
2022-06-17 11:18:06 -0300

    Feature: :sparkles: Active link

--- 7_react_router/src/components/Navbar.jsx
+++ 7_react_router/src/components/Navbar.jsx
@@ -1,14 +1,30 @@
 import './Navbar.css'
 
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
 	return (
 		<>
 			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/about">Sobre</Link>
+				{/*
+					<Link to="/">Home</Link>
+					<Link to="/about">Sobre</Link>
+				*/}
+
+				<NavLink 
+					to="/" 
+					// className={({isActive}) => (isActive ? 'active' : 'not-active')}
+				>
+					Home
+				</NavLink>
+
+				<NavLink 
+					to="/about" 
+					// className={({isActive}) => (isActive ? 'active' : 'not-active')}
+				>
+					Sobre
+				</NavLink>
 			</nav>	
 		</>
 	)
