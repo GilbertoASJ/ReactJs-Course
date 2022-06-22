62508fc3ea695db400c8b2f271565e0387913996
GilbertoASJ <alvesgilberto84@gmail.com>
2022-06-22 17:13:48 -0300

    Feature: :sparkles: Creating provider

--- 8_context_api/src/context/CounterContext.jsx
+++ 8_context_api/src/context/CounterContext.jsx
@@ -1,4 +1,17 @@
 // Criando contexto
 import { createContext, useState } from 'react'
 
-export default CounterContext = createContext()
+export const CounterContext = createContext()
+
+// Criando provider
+export const CounterContextProvider = ({children}) => {
+
+	let [counter, setCounter] = useState(5)
+
+	return (
+
+		<CounterContext.Provider value={{counter, setCounter}}>
+			{children}
+		</CounterContext.Provider>
+	)
+}
--- 8_context_api/src/pages/Home.jsx
+++ 8_context_api/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext'
+
 const Home = () => {
+
+	const { counter, setCounter } = useContext(CounterContext)
 
 	return (
 		<>
 			<h2>Home</h2>
+			<p>Valor do contador: {counter}</p>
 		</>
 	)
 }
