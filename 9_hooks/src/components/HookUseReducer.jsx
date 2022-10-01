0f1ed60ac5e757ef5d9d093de0f8d7c2d77c0309
GilbertoASJ <gilberto.sjunior@valid.com>
2022-10-01 11:43:48 -0300

    Feature: :sparkles: useReducer hook

--- 9_hooks/src/components/HookUseReducer.jsx
+++ 9_hooks/src/components/HookUseReducer.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 const HookUserReducer = () => {
 
@@ -8,11 +8,52 @@ const HookUserReducer = () => {
 		return Math.random(state);
 	});
 
+	// ----------------------------- //
+
+	// 2 - avançado com o useReducer
+	const initialTasks = [
+		{id: 1, text: 'Ir ao mercado'},
+		{id: 2, text: 'Limpar computador'}
+	]
+
+	const taskReducer = (state, action) => {
+
+	}
+
+	const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+	const [taskText, setTaskText] = useState("");
+
+	const handleSubmit = (e) => {
+
+		e.preventDefault();
+
+		dispatchTasks()
+	}
+
 	return (
 		<>
 			<h3>Use Reducer</h3>
 			<p>Número: {number}</p>
 			<button onClick={dispatch}>Alterar número</button>
+			<hr />
+
+			<h3>Todo List:</h3>
+
+			<form onSubmit={handleSubmit}>
+				<input 
+					type="text" 
+					onChange={(e) => setTaskText(e.target.value)} 
+					value={taskText} 
+				/>
+				<input type="submit" value="Enviar" />
+			</form>
+
+			{tasks.map((task) => (
+
+				<li key={task.id}>
+					{task.text}
+				</li>
+			))}
 		</>
 	)
 }
